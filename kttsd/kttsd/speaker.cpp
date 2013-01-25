cb3122eb0266e115ad0169c7df7ee38169d4861b
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2013-01-25 13:09:07 -0600

    Rename KServer, KSocket, KIO_EXPORT, KIOInput, KIOJob, KIOConfig, KIOBuffer, and KBuffer to avoid conflicts with KDE4

--- kttsd/kttsd/speaker.cpp
+++ kttsd/kttsd/speaker.cpp
@@ -1322,9 +1322,9 @@ uttIterator Speaker::deleteUtterance(uttIterator it)
                 d.rename(it->audioUrl, dest);
                 // TODO: This is always producing the following.  Why and how to fix?
                 // It moves the files just fine.
-                //  kio (KIOJob): stat file:///home/kde-devel/.trinity/share/apps/kttsd/audio/kttsd-5-1.wav
-                //  kio (KIOJob): error 11 /home/kde-devel/.trinity/share/apps/kttsd/audio/kttsd-5-1.wav
-                //  kio (KIOJob): This seems to be a suitable case for trying to rename before stat+[list+]copy+del
+                //  kio (TDEIOJob): stat file:///home/kde-devel/.trinity/share/apps/kttsd/audio/kttsd-5-1.wav
+                //  kio (TDEIOJob): error 11 /home/kde-devel/.trinity/share/apps/kttsd/audio/kttsd-5-1.wav
+                //  kio (TDEIOJob): This seems to be a suitable case for trying to rename before stat+[list+]copy+del
                 // TDEIO::move(it->audioUrl, dest, false);
             }
             else
